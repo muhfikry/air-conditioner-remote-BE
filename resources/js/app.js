08b415981352a79f7e872cf00b4b2d5292b45663
fajar-dev <fajarrivaldi2015@gmail.com>
2025-03-02 02:17:38 +0700

    fix: assets static

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,4 @@
 import '../css/style.bundle.css'
-import '../plugins/global/plugins.bundle.css'
+import '../css/global/plugins.bundle.css'
 // import './scripts.bundle.js'
 // import '../plugins/global/plugins.bundle.js'
